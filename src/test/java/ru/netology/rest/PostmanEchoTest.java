9cc3796f9bdb9c0c0c747a6008499df054d48e61
lifanova <veralifanovaa@gmail.com>
2021-05-12 10:56:01 +0300

    PostmanEchoTest: success check

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostToPostman() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"));
+    }
+}
